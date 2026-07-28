b1552bf8d76614e1bf180a23217e63d5a89bd257
mafilika <Mafilikaholdingsandinnovation@gmail.com>
2026-07-28 16:55:50 +0200

    Replace Firebase config placeholders with values
    Updated Firebase configuration with actual credentials.

--- app.js
+++ app.js
@@ -23,25 +23,12 @@ from
 // =================================
 
 const firebaseConfig = {
-
-  apiKey:
-  "PASTE_YOUR_API_KEY",
-
-  authDomain:
-  "PASTE_YOUR_AUTH_DOMAIN",
-
-  projectId:
-  "PASTE_YOUR_PROJECT_ID",
-
-  storageBucket:
-  "PASTE_YOUR_STORAGE_BUCKET",
-
-  messagingSenderId:
-  "PASTE_YOUR_MESSAGING_SENDER_ID",
-
-  appId:
-  "PASTE_YOUR_APP_ID"
-
+  apiKey: "AIzaSyAT6_OS3lgaXNGldWrpkV7by52cfWfpy9A",
+  authDomain: "wsw-event.firebaseapp.com",
+  projectId: "wsw-event",
+  storageBucket: "wsw-event.firebasestorage.app",
+  messagingSenderId: "1021036012955",
+  appId: "1:1021036012955:web:9712358aa7d69252126d3e"
 };
 
 
